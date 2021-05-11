cfc5d9566609ea76109557296d51ae48ddefb2ec
Pulkit3234 <pulkitsh08@gmail.com>
2021-05-11 15:49:54 +0530

    deploy -3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers/index';
